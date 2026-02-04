6b7e122ea11b0fef6d898125907445e50c69406c
Vadim <khalinvadim3@gmail.com>
2026-02-04 12:19:32 +0300

    Add User model in users/models.py; Set the project in settings.py Change language code, Time zone, delete comments, add in installed apps - rest framowork, add Secret key, debug and allowed hosts in .env)

---
+++ app/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+    first_name = models.CharField('Имя', max_length=100)
+    middle_name = models.CharField('Отчество', max_length=100, blank=True)
+    last_name = models.CharField('Фамилия', max_length=100, blank=True)
+    email = models.EmailField('Почта', unique=True)
+    is_active = models.BooleanField('Активный', default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
