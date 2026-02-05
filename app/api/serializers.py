33571da422ac2deb277fa1b4c7dda21688241e6f
Vadim <khalinvadim3@gmail.com>
2026-02-05 10:21:28 +0300

    Change UpdateSerializer in serializers.py;

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -18,10 +18,3 @@ class UpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('first_name', 'middle_name', 'last_name')
-
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.middle_name = validated_data.get('middle_name', instance.middle_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.save()
-        return instance
